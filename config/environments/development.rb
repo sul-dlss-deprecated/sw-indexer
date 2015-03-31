951aea4f91e62cb44c9d1de078e4e75ccd31c390
Laney McGlohon <laneymcg@stanford.edu>
2015-03-31 16:15:31 -0700

    Updated configuration files to put solr config and PURL_DEFAULT in application.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.solr_config_file_path = "#{config.root}/config/sw-solr.yml"
-
 end
-
-DiscoveryIndexer::PURL_DEFAULT='http://purl.stanford.edu/'
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,9 +39,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.solr_config_file_path = "#{config.root}/config/sw-solr.yml"
-
 end
 
-DiscoveryIndexer::PURL_DEFAULT='http://purl.stanford.edu/'
