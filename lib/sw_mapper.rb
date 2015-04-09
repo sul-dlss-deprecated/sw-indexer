0b6f226bdbe4316b941bad64dda59878514047fc
Laney McGlohon <laneymcg@stanford.edu>
2015-04-09 11:09:55 -0700

    Added fixtures for testing the SwMapper

--- lib/sw_mapper.rb
+++ lib/sw_mapper.rb
@@ -1,2 +1,85 @@
 class SwMapper < DiscoveryIndexer::Mapper::GeneralMapper
+
+  # You have access to these instance variables:
+  # @druid = object pid (no "druid:" prefix)
+  # @modsxml == Stanford::Mods::Record class object (@modsxml.mods_ng_xml == Nokogiri document (for custom parsing)
+  # @purlxml == DiscoveryIndexer::InputXml::PurlxmlModel class object (@purlxml.public_xml == Nokogiri document (for custom parsing)
+  # @collection_names == hash of collections the @druid belongs to (key = collection druid, value = collection name)
+  def convert_to_solr_doc()
+    solr_doc = super
+    solr_doc[:display_type] = display_type
+    solr_doc[:file_id] = file_ids
+#    solr_doc[:druid] = @druid
+#    solr_doc[:url_fulltext] = "http://purl.stanford.edu/#{@druid}"
+#    solr_doc[:access_facet] = 'Online'
+#    solr_doc[:building_facet] = 'Stanford Digital Repository'
+#    solr_doc[:collection] = collection
+    solr_doc[:collection_with_title] = collection_with_title
+#    solr_doc[:modsxml] = @modsxml.to_xml
+    solr_doc[:author_sort] = @modsxml.sw_sort_author
+    return solr_doc
+  end
+
+  # value is used to tell SearchWorks UI app of specific display needs for objects
+  # a config file value for add_display_type can be used to prepend a string to 
+  #  xxx_collection or xxx_object 
+  # e.g., Hydrus objects are a special display case
+  # Based on a value of :add_display_type in a collection's config yml file
+  # 
+  # information on DOR content types:
+  #   https://consul.stanford.edu/display/chimera/DOR+content+types%2C+resource+types+and+interpretive+metadata
+  #
+  # @return [String] 'collection' or DOR content type
+  def display_type
+    case @purlxml.dor_content_type
+      when 'book'
+        'book'
+      when 'image', 'manuscript', 'map'
+        'image'
+      else
+        'file'
+    end
+  end
+
+  # the @id attribute of resource/file elements that match the display_type, including extension
+  # @return [Array<String>] filenames
+  def file_ids
+    @file_ids ||= begin
+      ids = []
+      if display_type == "image"
+        ids = @purlxml.image_ids
+      elsif display_type == "file"
+        ids = @purlxml.file_ids
+      end
+      return nil if ids.empty?
+      ids
+    end
+  end
+
+  # the collection druid for items in a collection
+  # @return [Array<String>] druids
+  def collection
+    @collection ||= begin
+      coll_druids = []
+      @purlxml.collection_druids.each do | cdruid |
+        coll_druids.push cdruid
+      end
+      return nil if coll_druids.empty?
+      coll_druids
+    end
+  end
+
+  # The collection druid and collection title for items in a collection
+  # for display in SearchWorks
+  # @return [Array<String>] druid -|- title
+  def collection_with_title
+    @collection_with_title ||= begin
+      coll_title = []
+      @collection_names.each do | key, value|
+        coll_title.push "#{key}-|-#{value}"
+      end
+      return nil if coll_title.empty?
+      coll_title
+    end
+  end
 end
--- spec/sw_mapper_spec.rb
+++ spec/sw_mapper_spec.rb
@@ -1,6 +1,65 @@
 require 'rails_helper'
 
 describe SwMapper do
-	it "should properly map a SearchWorks digital object" do
+
+  context "No catkey" do
+	it "should properly map a digital object for SearchWorks" do
+	  setup('zz999zz9999','item_mods.xml','item_public.xml')
+
+	  expected_doc_hash =
+        {
+           :all_search => " Item title Personal name Role still image 1909 1915 Collection Title http://purl.stanford.edu/oo000oo0000 Access Condition ",
+           :author_1xx_search => nil,
+           :author_7xx_search => ["Personal name"],
+           :author_corp_display => [],
+           :author_meeting_display => [],
+	       :author_other_facet => [],
+	       :author_person_display => ["Personal name"],
+	       :author_person_facet => ["Personal name"],
+	       :author_person_full_display => ["Personal name"],
+	       :author_sort => " Item title",
+	       :creation_year_isi => "1909",
+	       :era_facet => nil,
+	       :format => ["Image"],
+	       :format_main_ssim => ["Image"],
+	       :geographic_facet => nil,
+	       :geographic_search => nil,
+	       :id => "zz999zz9999",
+	       :imprint_display => "1909",
+	       :language => [],
+	       :physical => nil,
+	       :pub_date => "1909",
+	       :pub_date_display => "1909",
+	       :pub_date_sort => "1909",
+	       :pub_search => nil,
+	       :pub_year_tisim => "1909",
+	       :subject_all_search => nil,
+	       :subject_other_search => nil,
+	       :subject_other_subvy_search => nil,
+	       :summary_search => nil,
+	       :title_245_search => "Item title.",
+	       :title_245a_display => "Item title",
+	       :title_245a_search => "Item title",
+	       :title_display => "Item title",
+	       :title_full_display => "Item title.",
+	       :title_sort => "Item title",
+	       :title_variant_search => [],
+	       :toc_search => nil,
+	       :topic_facet => nil,
+	       :topic_search => nil,
+	       :url_suppl => nil,
+	       :display_type => "image",
+	       :access_facet => "Online",
+	       :building_facet => "Stanford Digital Repository",
+	       :druid => "zz999zz9999",
+	       :file_id => ["a24.jp2", "a25.jp2", "a26.jp2", "a27.jp2", "a28.jp2"],
+	       :url_fulltext => "http://purl.stanford.edu/zz999zz9999",
+	       :collection => ["oo000oo0000"],
+	       :collection_with_title => ["oo000oo0000-|-Collection Name"],
+	       :modsxml => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<mods xmlns=\"http://www.loc.gov/mods/v3\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"3.3\" xsi:schemaLocation=\"http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-3.xsd\">\n  <titleInfo>\n    <title>Item title</title>\n  </titleInfo>\n  <name type=\"personal\">\n    <namePart>Personal name</namePart>\n    <role>\n      <roleTerm authority=\"marcrelator\" type=\"text\">Role</roleTerm>\n    </role>\n  </name>\n  <typeOfResource>still image</typeOfResource>\n  <originInfo>\n    <dateCreated point=\"start\" keyDate=\"yes\">1909</dateCreated>\n    <dateCreated point=\"end\">1915</dateCreated>\n  </originInfo>\n  <relatedItem type=\"host\">\n    <titleInfo>\n      <title>Collection Title</title>\n    </titleInfo>\n    <identifier type=\"uri\">http://purl.stanford.edu/oo000oo0000</identifier>\n    <typeOfResource collection=\"yes\"/>\n  </relatedItem>\n  <accessCondition type=\"copyright\">Access Condition</accessCondition>\n</mods>\n"
+        }
+
+      expect(@mapper.convert_to_solr_doc).to eq(expected_doc_hash)
 	end
+  end
 end
