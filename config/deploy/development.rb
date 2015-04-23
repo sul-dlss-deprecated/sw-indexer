e2d9febf2cd3529578931dfca2e621762e4a4ed3
Laney McGlohon <laneymcg@stanford.edu>
2015-04-23 12:20:00 -0700

    Updated gitignore with config files

--- config/deploy/development.rb
+++
@@ -1,7 +0,0 @@
-server 'sw-indexing-dev.stanford.edu', user: 'harvestdor', roles: %w{web app db}
-
-set :bundle_without, %w{deployment test}.join(' ')
-set :rails_env, "development"
-
-Capistrano::OneTimeKey.generate_one_time_key!
-
--- config/deploy/production.rb
+++
@@ -1,4 +0,0 @@
-set :bundle_without, %w{deployment test development staging}.join(' ')
-set :rails_env, "production"
-
-Capistrano::OneTimeKey.generate_one_time_key!
